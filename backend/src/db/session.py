88b6cbc17fd883c1996c1cf190780cd56145b0dc
ALbertE03 <marichalalberto292@gmail.com>
2026-06-20 13:47:16 -0400

    populate db

--- backend/src/db/session.py
+++ backend/src/db/session.py
@@ -2,7 +2,13 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import declarative_base
 from src import config
 
-engine = create_async_engine(config.DATABASE_URL)
+engine = create_async_engine(
+    config.DATABASE_URL,
+    pool_size=10,
+    max_overflow=20,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+)
 
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
 
